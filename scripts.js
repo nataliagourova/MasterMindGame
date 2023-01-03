7a62b1eba910c740dbb05adaccff3cf3386213d3
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2023-01-03 14:49:07 -0800

    comment cleanup

--- scripts.js
+++ scripts.js
@@ -1,42 +1,23 @@
-//obtain winning number set
-let secretNum = new Array (4);
 //generate 2D array to store guesses and results
-let guessNum = Array.from(Array(3), () => new Array (10));
+let guessNum = Array.from(Array(10), () => new Array (3));
 console.log(guessNum);
 //input got moved from inside the checkAnswers() function - move back after 2d array is functional
 let input = new Array (4);  
 //results for single attempt moved from inside selection() function - move back after 2d array is functional
 let digitsGuessed = 0;
 let positionsGuessed = 0;
-
+//initiate array to hold the winning number set
+let secretNum = new Array (4);
+//obtain the winning number set from API
 fetch('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new')
 .then ((response) => response.text())
 .then((data) => {
     secretNum  = data.trim().split('\n');
-    console.log(`set to guess: ${secretNum}`);
-    console.log(typeof data);
-    console.log(typeof secretNum);
-    console.dir(`secret number set: ${secretNum}`);    
+    console.log(`set to guess/loaded: ${secretNum}`);
+    console.log(`value type: ${typeof data}`);
+    console.log(`data type: ${typeof secretNum}`); 
 });
-console.dir(`secret number set: ${JSON.stringify(secretNum)}`);
-
-//TABLE
-
-// var mytable = "<table><tr>";
-// for (var CELL of ARRAY) {  mytable += "<td>" + CELL + "</td>"; }
-// mytable += "</tr></table>";
-// document.getElementById("ID").innerHTML = mytable; 
-
-// let table = document.createElement('table');
-// let row = table.insertRow();
-// row.textContent = "Your input";
-// let cell = row.insertCell();
-// cell.textContent = "New Cell!";
-// document.body.appendChild(table);
-
-// console.dir(table.rows);
-
-//function to compare user input to the secret winning number set
+console.dir(`starter number set: ${JSON.stringify(secretNum)}`);
 
 //add a row to User Input Log table
 function addRow(tableID, rowIndex, userInput, digitsGuessed, positionsGuessed) {
@@ -51,60 +32,32 @@ function addRow(tableID, rowIndex, userInput, digitsGuessed, positionsGuessed) {
     let cellTwo =newRow.insertCell(1).appendChild(document.createTextNode(userInput.toString()))
     let cellThree = newRow.insertCell(2).appendChild(document.createTextNode(digitsGuessed));
     let cellFour = newRow.insertCell(3).appendChild(document.createTextNode(positionsGuessed)); 
-    // let textOne = document.createTextNode(rowIndex);
-    // cellOne.appendChild(textOne);
-  }
+}
 
-  //collect user input as part of one attempt
+//collects user input as part of one attempt
 function selection (input){
-//     const first = document.getElementById("1num");
-//     console.log(`input 1num is ${first.value}`);
-//     input[0] = first.value;
-//     // first.addEventListener("input", () => {
-//     //     input.first = first.value;
-//     // });
-//     console.log(`first num: ${input[0]}`);
-
     input[0] = document.getElementById("1num").value;
     console.log(`first num: ${input[0]}`);
-
-    // const second = document.getElementById("2num");
-    // console.log(`input 2num is ${second.value}`);
-    // input[1] = second.value;   
-    // console.log(`second num: ${input[1]}`);    
+ 
     input[1] = document.getElementById("2num").value;
     console.log(`second num: ${input[1]}`);  
 
-    // const third = document.getElementById("3num");
-    // console.log(`input 3num is ${third.value}`);
-    // input[2] = third.value;  
-    // console.log(`third num: ${input[2]}`);  
-
     input[2] = document.getElementById("3num").value;
     console.log(`third num: ${input[2]}`);  
-
-    // const fourth = document.getElementById("4num");
-    // console.log(`input 4num is ${fourth.value}`);
-    // input[3] = fourth.value;   
-    // console.log(`fourth num: ${input[3]}`);
 
     input[3] = document.getElementById("4num").value;
     console.log(`fourth num: ${input[3]}`);
     return input;
 }
 
+//checks user input against the API generated number set
 function checkAnswers(){
     // let input = new Array (4);  
     console.log(`starter user input array: ${input}`);
-    // function to collect user input
-
+   
     selection(input);
     console.log(`user input number set: ${input}`);
-    // console.log(JSON.stringify(secretNum) == JSON.stringify(input));
-    // if (JSON.stringify(secretNum) == JSON.stringify(input)) {
-    //     alert('Congratulations, You Won!');
-    //     return;
-    // }
+
     let guesses = 10;
     //start the guess check loop
     for (let i=0; i<1; i++ ) {
@@ -141,15 +94,3 @@ function checkAnswers(){
     }
 
 }
-// const api = 'https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=html&rnd=new';
-
-
- 
-
-
-// async function getRandomInt(url) {
-//     const response = await fetch(url);
-//     let data = await response.json();
-//     console.log(data);
-// }
-// getRandomInt(api);
